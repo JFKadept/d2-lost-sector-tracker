60be81f1969d98622f6284f7cb4eb0bc1a7358fb
Aron Høyer <aron@hoyer.com>
2022-12-18 22:47:51 +0100

    add utility function to get unique destiny entities from a given list

---
+++ rotation_seeder/lib/get_unique_destiny_entities.ts
@@ -0,0 +1,8 @@
+import { DestinyEntity } from "../types/DestinyEntity.ts";
+
+export const getUniqueDestinyEntities = <T extends DestinyEntity>(entities: T[], entity: T): T[] => {
+    return entities.find((e) => e.hash === entity.hash)
+        ? entities
+        : [...entities, entity]
+}
+
